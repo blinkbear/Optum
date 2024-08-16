bf26de201e90efe5ae6ebe669b78de165fb7df76
NickLin <nicklin9907@gmail.com>
2024-08-16 02:36:57 +0000

    Merge with expriments scripts

---
+++ experiments/collector.py
@@ -0,0 +1,392 @@
+from AEFM.data_collector.jaeger_trace_collector import JaegerTraceCollector
+from AEFM.data_collector.models import CpuUsage, MemUsage, TestCaseData
+from AEFM.data_collector.wrk_throughput_collector import WrkThroughputCollector
+import pandas as pd
+from AEFM.data_collector.base import BaseDataCollector, try_except, ToBeSavedData
+from AEFM.utils.logger import log
+from AEFM.data_collector.prom_hardware_collector import PromHardwareCollector
+from AEFM.models import Node
+from statistics import mean
+
+
+class MyPromCollector(PromHardwareCollector):
+    def collect_node_cpu(self, nodes: list[Node], start_time, end_time):
+        ips = [node.ip for node in nodes]
+        constraint = ":9100|".join(ips) + ":9100"
+        query = f'instance:node_cpu_utilisation:rate5m{{instance=~"{constraint}"}}'
+        response = self.fetcher.fetch(query, "range", 1, start_time, end_time)
+        log.debug(
+            f"{__file__}: Fetch Node CPU usage from: {response.url}", to_file=True
+        )
+        usage = response.json()
+        records = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                ip = str(data["metric"]["instance"]).split(":")[0]
+                usage = max([float(v[1]) for v in data["values"]])
+                node = [n.name for n in nodes if n.ip == ip][0]
+                records.append({"node": node, "node_cpu": usage})
+        return pd.DataFrame(records)
+
+    def collect_node_mcp(self, nodes: list[Node], start_time, end_time):
+        ips = [node.ip for node in nodes]
+        constraint = ":9100|".join(ips) + ":9100"
+        query = f'instance:node_memory_utilisation:ratio{{instance=~"{constraint}"}}'
+        response = self.fetcher.fetch(query, "range", 1, start_time, end_time)
+        log.debug(
+            f"{__file__}: Fetch Node MCP usage from: {response.url}", to_file=True
+        )
+        usage = response.json()
+        records = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                ip = str(data["metric"]["instance"]).split(":")[0]
+                usage = max([float(v[1]) for v in data["values"]])
+                node = [n.name for n in nodes if n.ip == ip][0]
+                records.append({"node": node, "node_mcp": usage})
+        return pd.DataFrame(records)
+
+    def collect_node_net(self, nodes: list[Node], start_time, end_time):
+        ips = [node.ip for node in nodes]
+        constraint = ":9100|".join(ips) + ":9100"
+        query = f'sum(irate(node_network_transmit_bytes_total{{instance=~"{constraint}"}}[1m])) by (instance) / 1024 / 1024'
+        response = self.fetcher.fetch(query, "range", 1, start_time, end_time)
+        log.debug(
+            f"{__file__}: Fetch Node Net usage from: {response.url}", to_file=True
+        )
+        usage = response.json()
+        records = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                ip = str(data["metric"]["instance"]).split(":")[0]
+                usage = max([float(v[1]) for v in data["values"]])
+                node = [n.name for n in nodes if n.ip == ip][0]
+                records.append({"node": node, "node_net": usage})
+        return pd.DataFrame(records)
+
+    def collect_node_psi(self, nodes: list[Node], start_time, end_time):
+        query = f'node_psi{{node=~"{"|".join([x.name for x in nodes])}"}}'
+        response = self.fetcher.fetch(query, "range", 1, start_time, end_time)
+        log.debug(
+            f"{__file__}: Fetch Node PSI usage from: {response.url}", to_file=True
+        )
+        usage = response.json()
+        results = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                node = str(data["metric"]["node"])
+                resource = str(data["metric"]["resource"])
+                type = str(data["metric"]["type"])
+                window = str(data["metric"]["window"])
+                value = mean([float(v[1]) for v in data["values"]])
+                results.append(
+                    {
+                        "node": node,
+                        "resource": resource,
+                        "type": type,
+                        "window": window,
+                        "value": value,
+                    }
+                )
+        results = pd.DataFrame(results)
+        node_names = [x.name for x in nodes]
+        processed_results = []
+        for node_name in node_names:
+            data = results.loc[results["node"] == node_name]
+            data = data.sort_values(["resource", "type", "window"])
+            data["name"] = data["resource"] + "_" + data["type"] + "_" + data["window"]
+            data = {x: y for x, y in zip(data["name"].tolist(), data["value"].tolist())}
+            data["node"] = node_name
+            processed_results.append(data)
+        return pd.DataFrame(processed_results)
+
+    def collect_cpu_usage(
+        self, microservices: list[str], start_time: float, end_time: float
+    ) -> CpuUsage:
+        microservices.append("pythonpi")
+        constraint = 'pod=~"' + ".*|".join(microservices) + '.*"'
+        # ! Hardcode, 0.1 = pod CPU requests/limits
+        query = f"irate(container_cpu_usage_seconds_total{{{constraint}}}[1m]) / 1.5"
+        response = self.fetcher.fetch(
+            query, "range", step=1, start_time=start_time, end_time=end_time
+        )
+        log.debug(f"{__file__}: Fetch CPU usage from: {response.url}", to_file=True)
+        usage = response.json()
+        cpu_usage = CpuUsage()
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                pod = str(data["metric"]["pod"])
+                microservice = "-".join(pod.split("-")[:-2])
+                value = max([float(v[1]) for v in data["values"]])
+                cpu_usage.set(microservice, pod, value)
+        return cpu_usage
+
+    def collect_mem_usage(
+        self, microservices: list[str], start_time: float, end_time: float
+    ) -> MemUsage:
+        microservices.append("pythonpi")
+        constraint = 'pod=~"' + ".*|".join(microservices) + '.*"'
+        # ! Hardcode, 100 = pod mem requests/limits
+        query = f"container_memory_usage_bytes{{{constraint}}} / 1024 / 1024 / 200"
+        response = self.fetcher.fetch(
+            query, "range", step=1, start_time=start_time, end_time=end_time
+        )
+        log.debug(f"{__file__}: Fetch Mem usage from: {response.url}", to_file=True)
+        usage = response.json()
+        mem_usage = MemUsage()
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                pod = str(data["metric"]["pod"])
+                microservice = "-".join(pod.split("-")[:-2])
+                value = max([float(v[1]) for v in data["values"]])
+                mem_usage.set(microservice, pod, value)
+        return mem_usage
+
+    def collect_pod_cpu_psi(
+        self, microservices: list[str], start_time: float, end_time: float
+    ) -> pd.DataFrame:
+        microservices.append("pythonpi")
+        constraint = 'window!="total",pod_name=~"' + ".*|".join(microservices) + '.*"'
+        query = f"psi_perf_monitor_monitored_cpu_psi{{{constraint}}}"
+        response = self.fetcher.fetch(
+            query, "range", step=1, start_time=start_time, end_time=end_time
+        )
+        log.debug(f"{__file__}: Fetch pod CPU PSI from: {response.url}", to_file=True)
+        usage = response.json()
+        results = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                pod = str(data["metric"]["pod_name"])
+                microservice = "-".join(pod.split("-")[:-2])
+                type = str(data["metric"]["type"])
+                window = "avg" + str(data["metric"]["window"])[:-1]
+                value = mean([float(v[1]) for v in data["values"]])
+                results.append(
+                    {
+                        "microservice": microservice,
+                        "pod": pod,
+                        "type": type,
+                        "window": window,
+                        "value": value,
+                    }
+                )
+        results = pd.DataFrame(results)
+        pod_names = results["pod"].unique().tolist()
+        processed_results = []
+        for pod_name in pod_names:
+            data = results.loc[results["pod"] == pod_name]
+            data = data.sort_values(["type", "window"])
+            data["name"] = "cpu_" + data["type"] + "_" + data["window"]
+            data = {x: y for x, y in zip(data["name"].tolist(), data["value"].tolist())}
+            data["pod"] = pod_name
+            processed_results.append(data)
+        return pd.DataFrame(processed_results)
+
+    def collect_pod_mem_psi(
+        self, microservices: list[str], start_time: float, end_time: float
+    ) -> pd.DataFrame:
+        microservices.append("pythonpi")
+        constraint = 'window!="total",pod_name=~"' + ".*|".join(microservices) + '.*"'
+        query = f"psi_perf_monitor_monitored_mem_psi{{{constraint}}}"
+        response = self.fetcher.fetch(
+            query, "range", step=1, start_time=start_time, end_time=end_time
+        )
+        log.debug(f"{__file__}: Fetch pod Mem PSI from: {response.url}", to_file=True)
+        usage = response.json()
+        results = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                pod = str(data["metric"]["pod_name"])
+                microservice = "-".join(pod.split("-")[:-2])
+                type = str(data["metric"]["type"])
+                window = "avg" + str(data["metric"]["window"])[:-1]
+                value = mean([float(v[1]) for v in data["values"]])
+                results.append(
+                    {
+                        "microservice": microservice,
+                        "pod": pod,
+                        "type": type,
+                        "window": window,
+                        "value": value,
+                    }
+                )
+        results = pd.DataFrame(results)
+        pod_names = results["pod"].unique().tolist()
+        processed_results = []
+        for pod_name in pod_names:
+            data = results.loc[results["pod"] == pod_name]
+            data = data.sort_values(["type", "window"])
+            data["name"] = "memory_" + data["type"] + "_" + data["window"]
+            data = {x: y for x, y in zip(data["name"].tolist(), data["value"].tolist())}
+            data["pod"] = pod_name
+            processed_results.append(data)
+        return pd.DataFrame(processed_results)
+
+    def collect_pod_io_psi(
+        self, microservices: list[str], start_time: float, end_time: float
+    ) -> pd.DataFrame:
+        microservices.append("pythonpi")
+        constraint = 'window!="total",pod_name=~"' + ".*|".join(microservices) + '.*"'
+        query = f"psi_perf_monitor_monitored_io_psi{{{constraint}}}"
+        response = self.fetcher.fetch(
+            query, "range", step=1, start_time=start_time, end_time=end_time
+        )
+        log.debug(f"{__file__}: Fetch pod IO PSI from: {response.url}", to_file=True)
+        usage = response.json()
+        results = []
+        if usage["data"] and usage["data"]["result"]:
+            for data in usage["data"]["result"]:
+                pod = str(data["metric"]["pod_name"])
+                microservice = "-".join(pod.split("-")[:-2])
+                type = str(data["metric"]["type"])
+                window = "avg" + str(data["metric"]["window"])[:-1]
+                value = mean([float(v[1]) for v in data["values"]])
+                results.append(
+                    {
+                        "microservice": microservice,
+                        "pod": pod,
+                        "type": type,
+                        "window": window,
+                        "value": value,
+                    }
+                )
+        results = pd.DataFrame(results)
+        pod_names = results["pod"].unique().tolist()
+        processed_results = []
+        for pod_name in pod_names:
+            data = results.loc[results["pod"] == pod_name]
+            data = data.sort_values(["type", "window"])
+            data["name"] = "io_" + data["type"] + "_" + data["window"]
+            data = {x: y for x, y in zip(data["name"].tolist(), data["value"].tolist())}
+            data["pod"] = pod_name
+            processed_results.append(data)
+        return pd.DataFrame(processed_results)
+
+
+class MyDataCollector(BaseDataCollector):
+    def __init__(
+        self,
+        data_path: str,
+        trace_collector: JaegerTraceCollector,
+        hardware_collector: MyPromCollector,
+        throughput_collector: WrkThroughputCollector,
+        nodes: list[Node],
+        max_processes: int = 10,
+    ) -> None:
+        super().__init__(
+            data_path,
+            trace_collector,
+            hardware_collector,
+            throughput_collector,
+            max_processes,
+        )
+        self.nodes = nodes
+
+    @try_except("Collect node data")
+    def collect_node(self, test_case_data: TestCaseData) -> pd.DataFrame:
+        assert isinstance(self.hardware_collector, MyPromCollector)
+
+        cpu = self.hardware_collector.collect_node_cpu(
+            self.nodes, test_case_data.start_time, test_case_data.end_time
+        )
+        mcp = self.hardware_collector.collect_node_mcp(
+            self.nodes, test_case_data.start_time, test_case_data.end_time
+        )
+        net = self.hardware_collector.collect_node_net(
+            self.nodes, test_case_data.start_time, test_case_data.end_time
+        )
+        psi = self.hardware_collector.collect_node_psi(
+            self.nodes, test_case_data.start_time, test_case_data.end_time
+        )
+        return cpu.merge(mcp).merge(net).merge(psi)
+
+    @try_except("Collect JCT")
+    def collect_jct(self, test_case_data: TestCaseData) -> pd.DataFrame:
+        # ! Hardcode below
+        with open(f"tmp/offline_job_understanding/{test_case_data.name}", "r") as file:
+            jct = float(file.readline())
+        driver_jct = pd.DataFrame([{"pod": "driver", "jct": jct, "task_id": 0, "node": "driver"}])
+        pod_jct = pd.read_csv(f"tmp/offline_job_understanding/{test_case_data.name}.pod_jct")
+        return pd.concat([driver_jct, pod_jct])
+
+    @try_except("Collect Pod PSI")
+    def collect_pod_psi(
+        self, test_case_data: TestCaseData, statistical_data: pd.DataFrame
+    ) -> pd.DataFrame:
+        assert isinstance(self.hardware_collector, MyPromCollector)
+
+        microservices = statistical_data["microservice"].dropna().unique().tolist()
+        start_time = test_case_data.start_time
+        end_time = test_case_data.end_time
+
+        pod_cpu_psi = self.hardware_collector.collect_pod_cpu_psi(
+            microservices, start_time, end_time
+        )
+        pod_mem_psi = self.hardware_collector.collect_pod_mem_psi(
+            microservices, start_time, end_time
+        )
+        pod_io_psi = self.hardware_collector.collect_pod_io_psi(
+            microservices, start_time, end_time
+        )
+        return pod_cpu_psi.merge(pod_mem_psi).merge(pod_io_psi)
+
+    def collect(self, test_case_data: TestCaseData) -> None:
+        """Collect throughput, traces and hardware resource usage data and save
+        them into files.
+
+        Args:
+            test_data (TestCaseData): Test case related data.
+        """
+        self.test_case_data = test_case_data
+
+        jct_data = self.collect_jct(test_case_data)
+        if jct_data is None:
+            return
+        log.debug("collect jct success", to_file=True)
+
+        throughput_data = self.collect_throughput(test_case_data)
+        if throughput_data is None:
+            return
+        log.debug("collect throughput success", to_file=True)
+
+        trace_data = self.collect_traces(test_case_data)
+        if trace_data is None:
+            return
+        log.debug("collect traces success", to_file=True)
+
+        raw_data = trace_data.raw_data
+        statistical_data = trace_data.statistical_data
+        end_to_end_data = trace_data.end_to_end_data
+
+        hardware_data = self.collect_hardware(test_case_data, statistical_data)
+        if hardware_data is None:
+            return
+        log.debug("collect pod hardware success", to_file=True)
+
+        pod_psi_data = self.collect_pod_psi(test_case_data, statistical_data)
+        if pod_psi_data is None:
+            return
+        log.debug("collect pod PSI success", to_file=True)
+
+        hardware_data = hardware_data.merge(pod_psi_data)
+
+        node_data = self.collect_node(test_case_data)
+        if node_data is None:
+            return
+        log.debug("collect node success", to_file=True)
+
+        data_list = [
+            ToBeSavedData(statistical_data, f"{self.data_path}/statistical_data.csv"),
+            ToBeSavedData(raw_data, f"{self.data_path}/raw_data.csv"),
+            ToBeSavedData(hardware_data, f"{self.data_path}/hardware_data.csv"),
+            ToBeSavedData(throughput_data, f"{self.data_path}/throughput_data.csv"),
+            ToBeSavedData(end_to_end_data, f"{self.data_path}/end_to_end_data.csv"),
+            ToBeSavedData(node_data, f"{self.data_path}/node_data.csv"),
+            ToBeSavedData(jct_data, f"{self.data_path}/jct_data.csv"),
+        ]
+        result = self.append_additional_and_save(
+            data_list, test_case_data.additional_columns
+        )
+        if result is None:
+            return
+        log.key(f"Data collection of {test_case_data.name} success!")
---
+++ experiments/offline_job.py
@@ -0,0 +1,89 @@
+import os, threading, queue, subprocess, re, time
+from AEFM.utils.files import delete_path, create_folder, write_to_file
+import pandas as pd
+
+
+class OfflineJobLauncher:
+    def __init__(self, output_path: str):
+        self.hdfs_commands = "hadoop fs -rm -R hdfs://cute-serval:30900/python/compute.py; hadoop fs -copyFromLocal /home/lcy/understanding/spark-3.5.0-bin-hadoop3/python-pi/compute.py hdfs://cute-serval:30900/python/compute.py"
+        self.worker_thread: None | threading.Thread = None
+        self.message_queue: None | queue.Queue = None
+        self.output_path = output_path
+        create_folder(output_path, delete=True)
+        self.run_command = """
+/home/lcy/spark-3.5.0-bin-hadoop3/bin/spark-submit \
+    --master k8s://https://172.169.8.178:6443 \
+    --deploy-mode cluster\
+    --name spark-pi \
+    --conf spark.executor.instances={} \
+    --conf spark.kubernetes.container.image=k.harbor.siat.ac.cn/cc/spark-py:spark-3.5.3.1 \
+    --conf spark.kubernetes.authenticate.caCertFile=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt  \
+    --conf spark.kubernetes.authenticate.oauthTokenFile=/var/run/secrets/kubernetes.io/serviceaccount/token  \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+    --conf spark.kubernetes.file.upload.path=/root \
+    --conf spark.executor.memory=1g \
+    --conf spark.executor.memoryOverhead=1g \
+    hdfs://172.169.8.178:30900/python/compute.py
+"""
+
+    def worker(self, run_command, test_case_name, message: queue.Queue):
+        proc = subprocess.Popen(run_command, stdout=subprocess.PIPE, shell=True)
+
+        # Wait until: python-pi is working + available to fetch pods
+        time.sleep(60)
+        pod_node_file = f"{self.output_path}/{test_case_name}.pod_node"
+        columns = "-o custom-columns=pod:.metadata.name,node:.spec.nodeName"
+        grep = "grep -v 'driver'"
+        awk = "awk '{print $1\",\"$2}'"
+        command = f"kubectl get pod -n default {columns} | {grep} | {awk} > '{pod_node_file}'"
+        os.system(command)
+
+        out, _ = proc.communicate()
+        content = out.decode("utf-8")
+        message.put(content)
+
+    def join(self, test_case_name: str):
+        self.worker_thread.join()
+        delete_path(f"{self.output_path}/{test_case_name}")
+        delete_path(f"{self.output_path}/{test_case_name}.log")
+
+        # Record the JCT provided by driver pod
+        message = self.message_queue.get()
+        driver_pod = re.search(r"pod name:\s*(.*)\n", message).group(1)
+        command = f"kubectl logs -n default {driver_pod}"
+        out, _ = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE
+        ).communicate()
+        logs = out.decode("utf-8")
+        jct = re.search(r"Job 0 finished.*\s(\d+\.\d+)\ss\n", logs).group(1)
+        write_to_file(f"{self.output_path}/{test_case_name}", f"{jct}\n")
+        write_to_file(f"{self.output_path}/{test_case_name}.log", f"{message}\n")
+
+        # Record the JCT of each task on each executor
+        pod_jct_csv = []
+        for line in logs.split("\n"):
+            pod_jct = re.search(r"Finished task .* \(TID (\d+)\) in (\d+) ms on .* \(executor (\d+)\)", line)
+            if pod_jct is not None:
+                task_id, jct, executor = pod_jct.groups()
+                pod_jct_csv.append({"task_id": task_id, "executor": executor, "jct": jct})
+        pod_jct_csv = pd.DataFrame(pod_jct_csv)
+
+        # Get node that the pod assigned to.
+        pod_node_file = f"{self.output_path}/{test_case_name}.pod_node"
+        pod_jct_file = f"{self.output_path}/{test_case_name}.pod_jct"
+        pod_node_csv = pd.read_csv(pod_node_file)
+        pod_node_csv = pod_node_csv.assign(executor=pod_node_csv["pod"].apply(lambda x: str(x).split("-")[-1]))
+
+        pod_jct_csv.merge(pod_node_csv).drop(columns="executor").to_csv(pod_jct_file, index=False)
+
+
+    def start(self, instances, test_case_name):
+        os.system(self.hdfs_commands)
+        run_command = self.run_command.format(str(instances))
+        message = queue.Queue()
+        worker_thread = threading.Thread(
+            target=self.worker, args=(run_command, test_case_name, message)
+        )
+        worker_thread.start()
+        self.worker_thread = worker_thread
+        self.message_queue = message
