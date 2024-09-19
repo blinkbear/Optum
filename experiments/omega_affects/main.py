625d92d6ed0ee15208a46332d0ce819b25ca7fda
NickLin <nicklin9907@gmail.com>
2024-09-19 02:10:29 +0000

    Test affects of online/offline weights

---
+++ experiments/omega_affects/main.py
@@ -0,0 +1,33 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="log/omega_affects.log",
+    filemode="a",
+    format="<%(asctime)s>[%(name)s]%(levelname)s-%(message)s",
+)
+
+from .handlers import *
+from AEFM.manager import manager
+from AEFM import set_config_file, set_log_level
+
+set_config_file("experiments/omega_affects/configs.yaml")
+set_log_level("info")
+
+from ..data import MODEL_ROOT, DATA_ROOT
+
+manager.data.set(
+    "ls_models",
+    {
+        "frontend": f"{MODEL_ROOT}/frontend.ls",
+        "geo": f"{MODEL_ROOT}/geo.ls",
+        "profile": f"{MODEL_ROOT}/profile.ls",
+        "rate": f"{MODEL_ROOT}/rate.ls",
+        "reservation": f"{MODEL_ROOT}/reservation.ls",
+        "search": f"{MODEL_ROOT}/search.ls",
+    },
+)
+manager.data.set("be_models", {"pythonpi": f"{MODEL_ROOT}/pythonpi.be"})
+manager.data.set("hardware_data", f"{DATA_ROOT}/hardware_data.csv")
+
+manager.run()
