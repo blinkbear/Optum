a13bf71942f9e67f6c4570ac1380b4a1411d739c
NickLin <nicklin9907@gmail.com>
2024-09-23 11:51:10 +0000

    Add overhead experiment

---
+++ experiments/overhead/main.py
@@ -0,0 +1,12 @@
+from .handlers import *
+from AEFM.manager import manager
+from AEFM import set_config_file, set_log_level
+
+# Using cgroup v1 to stop collecting PSI
+# set_config_file("experiments/overhead/psi_on.yaml")
+# Set /proc/sys/kernel/perf_cpu_time_max_percent to 1 to minimize CPI affects
+# set_config_file("experiments/overhead/cpi_on.yaml")
+set_config_file("experiments/overhead/all_off.yaml")
+set_log_level("info")
+
+manager.run()
